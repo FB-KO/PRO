b44ab545703a5e720357516b1e37371d6fa2f123
JAHID HASSAN <113418680+FB-KO@users.noreply.github.com>
2022-10-30 17:41:32 +0600

    Create PRO.py

---
+++ PRO.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    os.system('xdg-open https://facebook.com/groups/1165084594446611/')
+    import V3
+else:
+    exit('\033[1;31m[×] Sorry Device Not Support')
